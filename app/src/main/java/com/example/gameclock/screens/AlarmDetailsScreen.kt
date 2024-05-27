2048f2644dd32df4ebff5456f60be297702cd1c8
Numan52 <numan.demir@gmx.at>
2024-05-27 14:37:36 +0200

    added DayPicker in AlarmDetailsScreen

--- app/src/main/java/com/example/gameclock/screens/AlarmDetailsScreen.kt
+++ app/src/main/java/com/example/gameclock/screens/AlarmDetailsScreen.kt
@@ -6,14 +6,36 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -66,7 +88,7 @@ fun SetAlarmScreen(
             verticalArrangement = Arrangement.Top,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(32.dp)
+                .padding(20.dp)
         ) {
             Spacer(modifier = Modifier.height(16.dp))
 
@@ -74,9 +96,14 @@ fun SetAlarmScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
+            Box(
+                modifier = Modifier
+            ) {
+                DayPicker()
+
+            }
         }
     }
-
 }
 
 @Composable
@@ -103,6 +130,73 @@ fun TimePicker() {
     )
 }
 
+
+@Composable
+fun AlarmSettings() {
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .fillMaxWidth()
+    )
+    {
+
+    }
+}
+
+@Composable
+fun DayPicker() {
+    val daysOfWeek = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+    val selectedDays = remember { mutableStateOf(setOf<String>()) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        daysOfWeek.forEach { day ->
+            val isSelected = selectedDays.value.contains(day)
+            Button(
+                onClick = {
+                      if (isSelected) {
+                          selectedDays.value -= day
+                      } else {
+                          selectedDays.value += day
+                      }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent
+                ),
+                //elevation = ButtonDefaults.elevation(0.dp),
+                contentPadding = PaddingValues(horizontal = 6.dp),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .height(30.dp)
+                    .width(50.dp),
+
+                border = if (isSelected) BorderStroke(2.dp, Color.Blue) else BorderStroke(1.dp, Color.Transparent)
+
+
+            ) {
+                Text(
+                    text = day,
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AlarmTone() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+
+    ) {
+
+    }
+}
 
 
 @SuppressLint("ScheduleExactAlarm")
